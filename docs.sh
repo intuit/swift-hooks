8194543bc54537c7d02a631dedad4fe6a1cafed1
Harris Borawski <harris_borawski@intuit.com>
2022-04-05 11:08:55 -0700

    add hosting base path to docs

--- docs.sh
+++ docs.sh
@@ -4,6 +4,7 @@ swift run docc convert Sources/Hooks/Documentation.docc \
   --fallback-display-name Hooks \
   --fallback-bundle-identifier com.intuit.hooks \
   --additional-symbol-graph-dir .build/swift-docc-symbol-graphs \
-  --output-path Hooks.doccarchive
+  --output-path Hooks.doccarchive \
+  --hosting-base-path swift-hooks
 
 swift run docc process-archive transform-for-static-hosting Hooks.doccarchive
